b7c5b528da4f26e17afb7fea89e3f33c57dc32a4
Ioannis Koutras <ioannis.koutras@gmail.com>
2017-12-13 17:28:36 +0200

    home-manager: Use home-base.nix

---
+++ home/.config/nixpkgs/home-base.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+let
+
+  sysconfig = config.lib.myConfig;
+
+  desktopApps = with pkgs; [
+    calibre
+    copyq
+    gwenview
+    iosevka
+    roboto
+    filezilla
+    skrooge
+    vlc
+  ];
+
+in {
+
+  programs.home-manager.enable = true;
+  programs.home-manager.path = https://github.com/rycee/home-manager/archive/master.tar.gz;
+
+  home.packages = with pkgs; [
+    exa
+    httpie
+    sshfs
+    mpd
+    ncmpcpp
+    ncdu
+    ranger
+    youtube-dl
+  ] ++ pkgs.lib.optionals sysconfig.services.xserver.enable desktopApps;
+
+}
--- home/.config/nixpkgs/home.nix
+++ home/.config/nixpkgs/home.nix
@@ -1,37 +1,4 @@
-{ config, pkgs, ... }:
-
-let
-
-  sysconfig = config.lib.myConfig;
-
-  desktopApps = with pkgs; [
-    calibre
-    copyq
-    gwenview
-    iosevka
-    roboto
-    filezilla
-    skrooge
-    vlc
-  ];
-
-in {
-
-  lib = let nixos = import <nixpkgs/nixos> { system = config.nixpkgs.system; };
-  in { myConfig = nixos.config; };
-
-  programs.home-manager.enable = true;
-  programs.home-manager.path = https://github.com/rycee/home-manager/archive/master.tar.gz;
-
-  home.packages = with pkgs; [
-    exa
-    httpie
-    sshfs
-    mpd
-    ncmpcpp
-    ncdu
-    ranger
-    youtube-dl
-  ] ++ pkgs.lib.optionals sysconfig.services.xserver.enable desktopApps;
-
+{
+  imports = [ ./home-base.nix ];
+  lib.myConfig = import <nixpkgs/nixos> { system = config.nixpkgs.system; };
 }
