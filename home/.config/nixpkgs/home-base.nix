e9705448aa56e294463995ece8b5bad72ba4b73a
Ioannis Koutras <ioannis.koutras@gmail.com>
2018-01-12 15:04:15 +0200

    home-manager: add more packages

--- home/.config/nixpkgs/home-base.nix
+++ home/.config/nixpkgs/home-base.nix
@@ -7,12 +7,18 @@ let
   desktopApps = with pkgs; [
     calibre
     copyq
+    polybar
     gwenview
+    font-awesome-ttf
     iosevka
     roboto
+    siji
     filezilla
     skrooge
     vlc
+    gnome3.dconf
+    gnome3.vinagre
+    jetbrains.idea-ultimate
   ];
 
 in {
@@ -21,12 +27,16 @@ in {
   programs.home-manager.path = https://github.com/rycee/home-manager/archive/master.tar.gz;
 
   home.packages = with pkgs; [
+    direnv
     exa
     httpie
     sshfs
     mpd
     ncmpcpp
     ncdu
+    neomutt
+    pgcli
+    python3Packages.yapf
     ranger
     youtube-dl
   ] ++ pkgs.lib.optionals sysconfig.services.xserver.enable desktopApps;
