709ab90a5bb52b3c5a7d3fed97a68297486c3992
Ioannis Koutras <ioannis.koutras@gmail.com>
2019-03-03 17:43:44 +0100

    home-manager: setup overlays

--- home/.config/nixpkgs/home-base.nix
+++ home/.config/nixpkgs/home-base.nix
@@ -104,6 +104,9 @@ in {
   nixpkgs.config = import ./nixpkgs-config.nix;
   xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
 
+  nixpkgs.overlays = import ./nixpkgs-overlays.nix;
+  xdg.configFile."nixpkgs/overlays.nix".source = ./nixpkgs-overlays.nix;
+
   home.sessionVariables = {
     GIO_EXTRA_MODULES = "${pkgs.gvfs}/lib/gio/modules";
   };
